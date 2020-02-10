e59a74dc948fac891ff5950365b4aeb7ad490781
admin <admin@admindeMacBook-Pro.local>
2020-02-10 14:56:27 +0700

    ---

--- GenerateJSON/GenerateJSON/ViewController.m
+++ GenerateJSON/GenerateJSON/ViewController.m
@@ -19,10 +19,10 @@
     [super viewDidLoad];
     
     NSDictionary *dictPayload = @{@"pariwara_muncul_url":@"https://detik.com",
-                           @"pariwara_muncul_gbr":@"https://github.com/axisXL/Pariwara/blob/master/pariwara.png",
+                           @"pariwara_muncul_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/pariwara.png",
                            @"pariwara_muncul_flag":@(1),
                            @"pariwara_tetap_url":@"https://kompas.com",
-                           @"pariwara_tetap_gbr":@"https://github.com/axisXL/Pariwara/blob/master/pariwara_muncul.png",
+                           @"pariwara_tetap_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/pariwara_muncul.png",
                            @"pariwara_tetap_flag":@(1), @"versi":@"1.1"};
     
     NSError *error;
