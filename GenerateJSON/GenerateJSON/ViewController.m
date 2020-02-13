f8ab3c060076f7cd4c20f17c1121252f9907a36d
admin <admin@admindeMacBook-Pro.local>
2020-02-13 17:18:16 +0700

    Add Trace Route

--- GenerateJSON/GenerateJSON/ViewController.m
+++ GenerateJSON/GenerateJSON/ViewController.m
@@ -18,6 +18,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self TraceRoute];
+}
+
+
+
+
+- (void)TraceRoute {
+    NSDictionary *dictPayload = @{@"pariwara_muncul_url":@"https://detik.com",
+                           @"pariwara_muncul_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/pariwara_muncul.png",
+                           @"pariwara_muncul_flag":@(1),
+                           @"pariwara_tetap_url":@"https://kompas.com",
+                           @"pariwara_tetap_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/pariwara.png",
+                           @"pariwara_tetap_flag":@(1), @"versi":@"1.1"};
+    
+    NSError *error;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dictPayload options:NSJSONWritingPrettyPrinted error:&error];
+    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    
+    NSString *encrypted = [Helper encrypt:jsonString error:&error];
+    NSLog(@"encrypted: %@", encrypted);
+    NSLog(@"decrypted: %@", [Helper decrypt:encrypted error:&error]);
+}
+
+
+
+- (void)UtilityBlock {
     NSDictionary *dictPayload = @{@"pariwara_muncul_url":@"https://detik.com",
                            @"pariwara_muncul_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/pariwara_muncul.png",
                            @"pariwara_muncul_flag":@(1),
@@ -32,7 +58,7 @@
     NSString *encrypted = [Helper encrypt:jsonString error:&error];
     NSLog(@"encrypted: %@", encrypted);
     NSLog(@"decrypted: %@", [Helper decrypt:encrypted error:&error]);
-}
 
+}
 
 @end
