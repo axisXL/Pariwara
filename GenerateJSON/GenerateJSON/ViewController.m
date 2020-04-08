03788d1c45526430ed50d737235b9597a3ac6787
AxisXl2019 <axisxl2019@gmail.com>
2020-04-08 14:30:16 +0700

    --

--- GenerateJSON/GenerateJSON/ViewController.m
+++ GenerateJSON/GenerateJSON/ViewController.m
@@ -106,7 +106,7 @@
 
 - (void)DragAd {
     NSDictionary *dictPayload = @{@"pariwara_muncul_url":@"https://detik.com", @"pariwara_muncul_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/DragAd/pariwara_muncul.png",
-                           @"pariwara_muncul_flag":@(1),
+                           @"pariwara_muncul_flag":@(0),
                            @"pariwara_tetap_url":@"https://kompas.com",
                            @"pariwara_tetap_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/DragAd/pariwara.png",
                            @"pariwara_tetap_flag":@(1), @"versi":@"1.2"};
