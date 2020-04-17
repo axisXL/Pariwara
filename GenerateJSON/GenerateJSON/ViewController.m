c01c6bd2100f57743ddf625a7988db5042193801
AxisXl2019 <axisxl2019@gmail.com>
2020-04-17 10:26:05 +0700

    -

--- GenerateJSON/GenerateJSON/ViewController.m
+++ GenerateJSON/GenerateJSON/ViewController.m
@@ -21,9 +21,10 @@
 //    [self TraceTrack];
 //    [self TraceRoute];
 //    [self BetterTrace];
-    [self DragAd];
+//    [self DragAd];
+    [self YDownloader];
     
-    [self generateURL:@"https://raw.githubusercontent.com/axisXL/Pariwara/master/DragAd/Utility.txt"];
+    [self generateURL:@"https://raw.githubusercontent.com/axisXL/Pariwara/master/YDownloader/Utility.txt"];
 }
 
 - (void)generateURL:(NSString *)strURL {
@@ -103,13 +104,28 @@
 
 }
 
-
 - (void)DragAd {
     NSDictionary *dictPayload = @{@"pariwara_muncul_url":@"https://detik.com", @"pariwara_muncul_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/DragAd/pariwara_muncul.png",
                            @"pariwara_muncul_flag":@(1),
                            @"pariwara_tetap_url":@"https://kompas.com",
                            @"pariwara_tetap_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/DragAd/pariwara.png",
                            @"pariwara_tetap_flag":@(1), @"versi":@"1.1"};
+    
+    NSError *error;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dictPayload options:NSJSONWritingPrettyPrinted error:&error];
+    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    
+    NSString *encrypted = [Helper encrypt:jsonString error:&error];
+    NSLog(@"encrypted: %@", encrypted);
+    NSLog(@"decrypted: %@", [Helper decrypt:encrypted error:&error]);
+}
+
+- (void)YDownloader {
+    NSDictionary *dictPayload = @{@"pariwara_muncul_url":@"https://detik.com", @"pariwara_muncul_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/YDownloader/pariwara_muncul.png",
+                           @"pariwara_muncul_flag":@(1),
+                           @"pariwara_tetap_url":@"https://kompas.com",
+                           @"pariwara_tetap_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/YDownloader/pariwara.png",
+                           @"pariwara_tetap_flag":@(1), @"versi":@"1.0"};
     
     NSError *error;
     NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dictPayload options:NSJSONWritingPrettyPrinted error:&error];
