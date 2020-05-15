d984ca03b524197b36cd8024bc64c8e8819d76be
AxisXl2019 <axisxl2019@gmail.com>
2020-05-15 15:01:39 +0700

    --

--- GenerateJSON/GenerateJSON/ViewController.m
+++ GenerateJSON/GenerateJSON/ViewController.m
@@ -168,4 +168,20 @@
     NSLog(@"encrypted: %@", encrypted);
     NSLog(@"decrypted: %@", [Helper decrypt:encrypted error:&error]);
 }
+
+- (void)PhotoEditor {
+    NSDictionary *dictPayload = @{@"pariwara_muncul_url":@"https://detik.com", @"pariwara_muncul_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/RWeather/pariwara_muncul.png",
+                           @"pariwara_muncul_flag":@(1),
+                           @"pariwara_tetap_url":@"https://kompas.com",
+                           @"pariwara_tetap_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/RWeather/pariwara.png",
+                           @"pariwara_tetap_flag":@(1), @"versi":@"1.0"};
+    
+    NSError *error;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dictPayload options:NSJSONWritingPrettyPrinted error:&error];
+    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    
+    NSString *encrypted = [Helper encrypt:jsonString error:&error];
+    NSLog(@"encrypted: %@", encrypted);
+    NSLog(@"decrypted: %@", [Helper decrypt:encrypted error:&error]);
+}
 @end
