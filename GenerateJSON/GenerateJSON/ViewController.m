d2c798d36d5ab396c42da2203adcaf36736a4600
AxisXl2019 <axisxl2019@gmail.com>
2020-06-18 16:05:50 +0700

    --

--- GenerateJSON/GenerateJSON/ViewController.m
+++ GenerateJSON/GenerateJSON/ViewController.m
@@ -77,11 +77,11 @@
 
 - (void)Sleep {
     NSDictionary *dictPayload = @{@"pariwara_muncul_url":@"https://detik.com",
-                           @"pariwara_muncul_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/Express%20Run/pariwara_muncul.png",
-                           @"pariwara_muncul_flag":@(1),
-                           @"pariwara_tetap_url":@"https://kompas.com",
-                           @"pariwara_tetap_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/Express%20Run/pariwara.png",
-                           @"pariwara_tetap_flag":@(1), @"versi":@"1.0"};
+                                    @"pariwara_muncul_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/Sleep/pariwara_muncul.png",
+                                    @"pariwara_muncul_flag":@(1),
+                                    @"pariwara_tetap_url":@"https://kompas.com",
+                                    @"pariwara_tetap_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/Sleep/pariwara.png",
+                                    @"pariwara_tetap_flag":@(1), @"versi":@"1.0"};
     
     NSError *error;
     NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dictPayload options:NSJSONWritingPrettyPrinted error:&error];
@@ -94,10 +94,10 @@
 
 - (void)BetterTrace {
     NSDictionary *dictPayload = @{@"pariwara_muncul_url":@"https://detik.com",
-                           @"pariwara_muncul_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/Sleep/pariwara_muncul.png",
+                           @"pariwara_muncul_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/TraceTrack/pariwara_muncul.png",
                            @"pariwara_muncul_flag":@(1),
                            @"pariwara_tetap_url":@"https://kompas.com",
-                           @"pariwara_tetap_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/Sleep/pariwara.png",
+                           @"pariwara_tetap_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/TraceTrack/pariwara.png",
                            @"pariwara_tetap_flag":@(1), @"versi":@"1.1"};
     
     NSError *error;
