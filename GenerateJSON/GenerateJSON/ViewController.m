5e24510a44daed9b5a82772d9b397f608cd6f4d1
AxisXl2019 <axisxl2019@gmail.com>
2021-02-16 15:20:00 +0700

    --

--- GenerateJSON/GenerateJSON/ViewController.m
+++ GenerateJSON/GenerateJSON/ViewController.m
@@ -20,7 +20,7 @@
     
 //    [self TraceTrack];
 //    [self TraceRoute];
-//    [self BetterTrace];
+    [self BetterTrace];
 //    [self DragAd];
 //    [self GreenRecipe];
 //    [self UnlimitedFTP];
@@ -36,7 +36,7 @@
 //    [self BetterTrip];
 //    [self RealWeather];
 //    [self GoNetworkTools];
-    [self PhotoEditor];
+//    [self PhotoEditor];
 //    [self SmartAppLock];
     [self generateURL:@"https://raw.githubusercontent.com/axisXL/Pariwara/master/SmartAppLock/Utility.txt"];
 }
@@ -121,10 +121,10 @@
 - (void)BetterTrace {
     NSDictionary *dictPayload = @{@"paksaForever":@(1), @"pariwara_muncul_url":@"http://betterdownload.net",
                            @"pariwara_muncul_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/BetterTrace/pariwara_muncul.png",
-                           @"pariwara_muncul_flag":@(1),
+                           @"pariwara_muncul_flag":@(0),
                            @"pariwara_tetap_url":@"http://betterdownload.net",
                            @"pariwara_tetap_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/BetterTrace/pariwara.png",
-                           @"pariwara_tetap_flag":@(1), @"versi":@"1.3"};
+                           @"pariwara_tetap_flag":@(0), @"versi":@"1.3"};
     
     NSError *error;
     NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dictPayload options:NSJSONWritingPrettyPrinted error:&error];
@@ -333,10 +333,10 @@
 
 - (void)PhotoEditor {
     NSDictionary *dictPayload = @{@"iklanForever":@(1), @"pariwara_muncul_url":@"http://betterdownload.net", @"pariwara_muncul_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/Photo%20Editor/pariwara_muncul.png",
-                           @"pariwara_muncul_flag":@(1),
+                           @"pariwara_muncul_flag":@(0),
                            @"pariwara_tetap_url":@"http://betterdownload.net",
                            @"pariwara_tetap_gbr":@"https://raw.githubusercontent.com/axisXL/Pariwara/master/Photo%20Editor/pariwara.png",
-                           @"pariwara_tetap_flag":@(1), @"versi":@"1.2"};
+                           @"pariwara_tetap_flag":@(0), @"versi":@"1.2"};
     
     NSError *error;
     NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dictPayload options:NSJSONWritingPrettyPrinted error:&error];
